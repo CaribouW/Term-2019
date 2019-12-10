488b7b2a9442b82c95dcf6a120b5cf1f5e68a2be
CaribouW <1071956678@qq.com>
2019-12-10 10:16:44 +0800

    update front-express

--- front/backend/app.js
+++ front/backend/app.js
@@ -9,13 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//port
+app.listen(8000);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- front/backend/routes/index.js
+++ front/backend/routes/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.redirect('login.html');
-    res.end();
+    res.redirect('/login.html').end();
+});
+
+//多级回调
+router.get('/example/b', function (req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function (req, res) {
+    res.send('Hello from B!');
+});
+
+router.get('/getReq', function (req, res) {
+    res.sendStatus(404);
 });
 
 module.exports = router;

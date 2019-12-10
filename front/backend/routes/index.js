47c486e55318b221342f3534fa35465cee44e573
CaribouW <1071956678@qq.com>
2019-12-10 11:11:45 +0800

    add

--- front/backend/routes/index.js
+++ front/backend/routes/index.js
@@ -2,20 +2,8 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.redirect('/login.html').end();
-});
-
-//多级回调
-router.get('/example/b', function (req, res, next) {
-    console.log('the response will be sent by the next function ...');
-    next();
-}, function (req, res) {
-    res.send('Hello from B!');
-});
-
-router.get('/getReq', function (req, res) {
-    res.sendStatus(404);
+router.get('/', function (req, res) {
+    res.redirect('/login.html');
 });
 
 module.exports = router;
